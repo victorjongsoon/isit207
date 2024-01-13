56bf1b6785614a9dd976bb957efc9694e658f02f
Victor Jong <victor.jong@ncs.com.sg>
2024-01-13 17:01:47 +0800

    car return

---
+++ js/return.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get a reference to the "Upload" button
+    var uploadButton = document.getElementById('submitButton');
+
+    // Add a click event listener to the button
+    uploadButton.addEventListener('click', function () {
+        // Show the alert when the button is clicked
+        alert('You have succesfully return the car. Thank you!');
+    });
+});
+
